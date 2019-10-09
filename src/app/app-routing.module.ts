f80e9735b8b275f33c2fd441ef2124b4a37d26af
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-10-09 18:58:08 +0200

    Added edit page, class and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'edit', loadChildren: './pages/edit/edit.module#EditPageModule' },
+  { path: 'edit/:id', loadChildren: './pages/edit/edit.module#EditPageModule' },
 ];
 
 @NgModule({
