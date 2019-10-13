27c7e84e1ad4abafbb7861d5e89e85c75f76538d
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-10-13 19:58:05 +0200

    Several updated. Everything working in memory.

--- src/app/pages/edit/edit.page.ts
+++ src/app/pages/edit/edit.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/model/task';
 import { TasksService } from 'src/app/services/tasks.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -11,10 +12,16 @@ export class EditPage implements OnInit {
 
   task: Task = {title: '', description: ''};
 
-  constructor(private tasksService: TasksService) { }
+  constructor(
+    private tasksService: TasksService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id != null) {
+      this.task = this.tasksService.getTask(+id);
+    }
   }
 
   saveTask() {
